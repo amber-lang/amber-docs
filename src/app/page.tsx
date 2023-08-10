64824f5c1aea5ac716248bf5050785b5402d8345
Ph0enixKM <pkaras.it@gmail.com>
2023-08-10 16:21:12 +0200

    feat: add simple homescreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@
 
 import { useDocument } from '@/contexts/DocumentContext'
 import { useEffect } from 'react'
+import style from './page.module.css'
+import { getTableOfContents } from '@/utils/docs'
+import Link from 'next/link'
 
 export default function Post() {
   const { setDocument } = useDocument()
+  const [toc] = getTableOfContents()
 
   useEffect(() => {
     setDocument('')
@@ -12,7 +16,16 @@ export default function Post() {
 
   return (
     <>
-      Welcome to Home Page
+      <div className={style.container}>
+        <div className={style.jumbotron}></div>
+        <div className={style.title}>
+          <span className={style.bold}>Amber</span>
+          <span className={style.light}>Docs</span>
+        </div>
+        <Link href={toc.path} className={style['big-link']}>
+          {toc.title}
+        </Link>
+      </div>
     </>
   )
 }
