472f77943578c7c36d8af0512764c72005daec30
Ph0enixKM <pkaras.it@gmail.com>
2023-08-25 20:20:24 +0200

    fix: dark mode not toggling on mobile

--- src/components/SettingsGrid/SettingsGrid.tsx
+++ src/components/SettingsGrid/SettingsGrid.tsx
@@ -1,13 +1,22 @@
+'use client'
+
 import Icon from '../Icon/Icon'
 import Button from '../Button/Button'
 import style from './SettingsGrid.module.css'
+import { useTheme } from '@/contexts/ThemeContext'
 
 export default function SettingsGrid() {
+    const { mode, setThemeMode } = useTheme()
+
+    const toggleTheme = () => {
+        setThemeMode(mode === 'light' ? 'dark' : 'light')
+    }
+
     return (
         <div>
             <div className={style.buttons}>
                 <div className={style.button}>
-                    <Button>
+                    <Button onClick={toggleTheme}>
                         <Icon src='internal/moon.svg' size='2rem' />
                     </Button>
                 </div>
