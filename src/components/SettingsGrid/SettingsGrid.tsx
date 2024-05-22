4e3e70ce1e95fa46cd1fb3fcb72561631a4bcae0
Aaron Boushley <aaron@boushley.cc>
2024-05-22 07:37:13 -0700

    Update the SettingsGrid used on mobile to line up with navigation

--- src/components/SettingsGrid/SettingsGrid.tsx
+++ src/components/SettingsGrid/SettingsGrid.tsx
@@ -4,9 +4,11 @@ import Icon from '../Icon/Icon'
 import Button from '../Button/Button'
 import style from './SettingsGrid.module.css'
 import { useTheme } from '@/contexts/ThemeContext'
+import {useRouter} from 'next/navigation'
 
 export default function SettingsGrid() {
     const { mode, setThemeMode } = useTheme()
+    const router = useRouter()
 
     const toggleTheme = () => {
         setThemeMode(mode === 'light' ? 'dark' : 'light')
@@ -17,16 +19,22 @@ export default function SettingsGrid() {
             <div className={style.buttons}>
                 <div className={style.button}>
                     <Button onClick={toggleTheme}>
-                        <Icon src='/internal/moon.svg' size='2rem' />
+                        <Icon src='/internal/moon.svg' size='2rem'/>
                     </Button>
                 </div>
                 <div className={style.button}>
-                    <Button>
-                        <a href="https://marble.software/">
-                            <Icon src='/internal/marble.svg' size='2rem' />
-                        </a>
+                    <Button onClick={() => router.push('https://github.com/Ph0enixKM/Amber')}>
+                        <Icon src='/internal/gh.svg' size='2rem'/>
                     </Button>
                 </div>
+                {/* Uncomment when we are ready */}
+                {/* <div className={style.button}>
+                    <Button>
+                        <a href="https://marble.software/">
+                            <Icon src='/internal/marble.svg' size='2rem'/>
+                        </a>
+                    </Button>
+                </div> */}
             </div>
         </div>
     )
