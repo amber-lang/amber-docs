e878d512d7c2eb71d96dfcc86b04bf4a1ecc1e44
Paweł Karaś <pkaras.it@gmail.com>
2025-12-07 12:40:28 +0100

    fix: docs point to next chapter (#125)

--- src/utils/docs.ts
+++ src/utils/docs.ts
@@ -4,7 +4,10 @@ export type FlatDoc = { path: string, title: string, disableLevels?: number[] };
 export type DocDescriptor = { index: number } & FlatDoc;
 
 export function getFlatTableOfContents(toc: TocSection[]): FlatDoc[] {
-    return toc.map((doc: FlatDoc & { docs: FlatDoc[] }) => [doc, ...doc.docs]).flat() as FlatDoc[]
+    return toc.map((doc: FlatDoc & { docs: FlatDoc[] }) => {
+        const uniqueDocs = doc.docs.filter(d => d.path !== doc.path)
+        return [doc, ...uniqueDocs]
+    }).flat() as FlatDoc[]
 }
 
 export function getDocDescriptor(toc: TocSection[], path: string) {
