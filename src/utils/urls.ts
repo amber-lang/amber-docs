267002290211652c2e890d41c73098be74befa71
Paweł Karaś <pkaras.it@gmail.com>
2025-12-22 10:50:01 +0100

    feat: rename 0.6.0 to nightly (#166)

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -15,7 +15,7 @@ export interface Location {
 }
 
 export function getLocation (slug: string[]): Location {
-    const versionRegex = /^\d+\.\d+\.\d+(?:-(?:alpha|beta))?$/
+    const versionRegex = /^(?:\d+\.\d+\.\d+(?:-(?:alpha|beta))?|nightly(?:-alpha)?)$/
     if (versionRegex.test(slug[0])) {
         return {
             version: slug[0],
